a610b636bed65ec3fa5567d9a1991b47efee53e2
Guillaume <9uillaume@gmail.com>
2021-02-20 16:51:26 -0500

    improvments

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,26 +5,20 @@ import axios from 'axios'
 const IndexPage = () => {
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12 col-md-3">
-          <Button
-            confirm
-            interval={10000}
-            getStatus={async () => {
-              return axios.get(`/api/pi`).then(r => r.data).catch(() => ({ state : 'error', text: 'Pi Off'}))
-            }}
-            onClick={async () => {
-                axios.post(`/api/pi/reboot`);
-            }}
-          />
-        </div>
-        <div className="col-12 col-md-3">
-          <Button
-            getStatus={async () => axios.get(`/api/hdd`).then(r => r.data)}
-          />
-        </div>
-      </div>
+    <div className="container d-flex align-items-start justify-content-start">
+      <Button
+        confirm
+        interval={10000}
+        getStatus={async () => {
+          return axios.get(`/api/pi`).then(r => r.data).catch(() => ({ state : 'error', text: 'Pi Off'}))
+        }}
+        onClick={async () => {
+            axios.post(`/api/pi/reboot`);
+        }}
+      />
+      <Button
+        getStatus={async () => axios.get(`/api/hdd`).then(r => r.data)}
+      />
     </div>
   )
 }
